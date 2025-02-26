5606d63e04e980f5c75bbfbef1d737e87c883752
Thunyatep <taptitan4@gmail.com>
2025-02-26 14:35:23 +0700

    navbar

--- front/src/components/navbar.jsx
+++ front/src/components/navbar.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 
-const navbar = () => {
+export default function Navbar() {
   return (
-    <div>navbar</div>
+    <div className='bg-slate-500 p-5 text-white'>
+
+      <div className='flex justify-between items-center'>
+        <div className='flex gap-4'>
+          <span>Home</span>
+          <span>info</span>
+          <span>Machine Learning</span>
+          <span>Nural network</span>
+        </div>
+        <div>
+          Hand sign predic
+        </div>
+      </div>
+
+    </div>
   )
 }
-
-export default navbar
--- front/src/pages/Homepage.jsx
+++ front/src/pages/Homepage.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
-import { PulseLoader   } from 'react-spinners'
+import { useEffect, useState } from 'react'
+import { PulseLoader } from 'react-spinners'
+import Navbar from '@/components/Navbar'
 
 function Homepage() {
-  return (
-    <div className='flex justify-center w-screen'><PulseLoader  color='#ff793f'/></div>
+  const [load, setload] = useState(true)
+  useEffect(() => {
+    const time = setTimeout(() => {
+      setload(false)
+    }, 3000)
+    return (() => {
+      clearTimeout(time);
+    })
+  }, [])
+  return load ? <div className='flex justify-center items-center h-screen'><PulseLoader color='#ff793f' /></div> : (
+      <div className=''>
+      <Navbar/>
+      </div>  
   )
 }
 
